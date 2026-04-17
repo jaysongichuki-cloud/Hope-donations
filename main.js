f8cb850a24740d7ba90f93a9d6ad93647c59b383
jaysongichuki-cloud <jayson.gichuki@student.moringaschool.com>
2026-04-17 11:52:51 +0300

    Add files via upload
    added conversion rates api

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ cards.forEach(card => {
         selectedText.textContent = "Selected Cause: " + cause;
     });
 });
+
+const donateBtn = document.querySelector('.donation-form button');
+const statusText = document.getElementById('payment-status');
+
+donateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const amount = amountInput.value;
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/KES`)
+        .then(response => response.json())
+        .then(data => {
+            const rate = data.rates.USD;
+
+            const converted = amount * rate;
+
+            conversionResult.textContent = `Converted Amount: $${converted.toFixed(2)}`;
+    
+    statusText.textContent = "Processing your M-Pesa payment...";
+
+    setTimeout(() => {
+        statusText.textContent = "Payment successful! Thank you for your donation.";
+    }, 3000);
+});
+});
+const amountInput = document.getElementById('amount');
+const conversionResult = document.getElementById('conversion-result');
+
