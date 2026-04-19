1dc9166865d7fcacbf01d8654a3df165e6789c04
jaysongichuki-cloud <jayson.gichuki@student.moringaschool.com>
2026-04-19 11:45:08 +0300

    Add files via upload
    added jsonplaceholder

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+console.log("Js is running");
+console.log(document.getElementById("phone"));
+console.log(document.getElementById("donor-list"));
+
+let donations = [];
 const cards=document.querySelectorAll('.donate-card');
 const selectedText = document.getElementById('selected-cause');
+const amountInput = document.getElementById('amount');
+const conversionResult = document.getElementById('conversion-result');
+
+const totalDisplay = document.getElementById("total-donations")
+
+
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
@@ -15,6 +26,7 @@ donateBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
     const amount = amountInput.value;
+    const phone= document.getElementById("phone").value
 
     fetch(`https://api.exchangerate-api.com/v4/latest/KES`)
         .then(response => response.json())
@@ -24,14 +36,54 @@ donateBtn.addEventListener('click', (e) => {
             const converted = amount * rate;
 
             conversionResult.textContent = `Converted Amount: $${converted.toFixed(2)}`;
+
+            const selectedCause= document.getElementById("selected-cause").textContent;
+            const donation = {
+                cause: selectedCause,
+                amount: amount,
+                phone: phone
+            };
+
+donations.push(donation);
     
     statusText.textContent = "Processing your M-Pesa payment...";
 
     setTimeout(() => {
         statusText.textContent = "Payment successful! Thank you for your donation.";
+
+        const total=donations.reduce((sum,d)=>{
+    return sum + Number(d.amount);
+},0)
+
+totalDisplay.textContent = "Total Donations:" + total +"KES";
     }, 3000);
 });
 });
-const amountInput = document.getElementById('amount');
-const conversionResult = document.getElementById('conversion-result');
 
+
+
+
+const donorList = document.getElementById("donor-list");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(users=>{
+    users.slice(0,5).forEach(user=>{
+        const li = document.createElement("li");
+        li.textContent=user.name;
+
+        donorList.appendChild(li)
+    });
+});
+
+function addDonorToList(name,amount,cause){
+    const li = document.createElement("li");
+    li.textContent = `${name} - KES ${amount} - ${cause}`;
+    donorList.appendChild(li);
+}
+
+addDonorToList(
+    phone,
+    amount,
+    selectedCause.replace("Selected Cause: ","")
+);
